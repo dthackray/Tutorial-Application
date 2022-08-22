84a87181f9464dda4b5cbed798d9d42ba992f9cb
Dan Thackray <djthackray@gmail.com>
2022-08-22 15:24:48 +0100

    Initial server.js framework .get .use .listen

--- server.js
+++ server.js
@@ -6,3 +6,16 @@ let corsOptions = {
     origin: 'http://localhost:8081'
 }
 
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the application.' })
+})
+
+const PORT = process.env.port || 8080
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
