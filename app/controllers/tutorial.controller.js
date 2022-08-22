135a6e87c145f38ae4d70b7a087a9e9e1c8d35d5
Dan Thackray <djthackray@gmail.com>
2022-08-22 17:35:18 +0100

    Refactor, test all API routes using Postman

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -67,27 +67,29 @@ exports.findOne = (req, res) => {
 };
 // Update a Tutorial by the id in the request
 exports.update = (req, res) => {
-  const id = req.params.id
-  Tutorial.update(req.body), {
-    where: { id: id }
-  }
-    .then(num => {
-        if (num === 1) {
-            res.send({
-                message: "Tutorial was updated successfully"
-            })
-        } else {
-            res.send({
-                message: `Cannot update Tutorial with id ${id}`
-            })
-            }
-        })
-    .catch(err => {
-        res.status(500).send({
-            message: `Error updating Tutorial with id ${id}`
-        })
+    const id = req.params.id
+  
+    Tutorial.update(req.body, {
+      where: { id: id }
     })
-};
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Tutorial was updated successfully."
+          });
+        } else {
+          res.send({
+            message: `Cannot update Tutorial with id=${id}`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error updating Tutorial with id=" + id
+        });
+      });
+  };
+
 // Delete a Tutorial with the specified id in the request
 exports.delete = (req, res) => {
   const id = req.params.id
@@ -129,14 +131,14 @@ exports.deleteAll = (req, res) => {
 };
 // Find all published Tutorials
 exports.findAllPublished = (req, res) => {
-  Tutorial.findAll({ where: {published: true } })
-  .then(data => {
-    res.send(data)
-  })
-  .catch(err => {
-    res.status(500).send({
-        message:
-            err.message || "Some error occurred while retrieving published tutorials"
-    })
-  })
-};
+    Tutorial.findAll({ where: { published: true } })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving tutorials."
+        });
+      });
+  };
