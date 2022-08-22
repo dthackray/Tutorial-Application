cc9cfd317da7fa2d59cf5f7a4bb671d55add00af
Dan Thackray <djthackray@gmail.com>
2022-08-22 15:35:34 +0100

    Configure and initialise sequelize and force resyncs

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const db = require('./app/models')
 const app = express()
 let corsOptions = {
     origin: 'http://localhost:8081'
 }
 
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('Drop and re-sync db')
+})
+
 app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+
 app.get('/', (req, res) => {
-    res.json({ message: 'Welcome to the application.' })
+    res.json({ message: 'Welcome to the application' })
 })
 
 const PORT = process.env.port || 8080
